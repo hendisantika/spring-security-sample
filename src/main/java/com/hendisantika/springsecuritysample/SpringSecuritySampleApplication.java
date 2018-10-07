8be57800d1c86ba023e64f07fc564f5cb1130f69
Hendi Santika <hendisantika@gmail.com>
2018-10-08 03:41:00 +0700

    #1 - first commit

---
+++ src/main/java/com/hendisantika/springsecuritysample/SpringSecuritySampleApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springsecuritysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecuritySampleApplication.class, args);
+    }
+}
